da60044deaeb57181962b1a69c94da4e6dc3ce4d
monicadeshmukh <renavikar_monica@yahoo.com>
2020-07-16 12:51:43 -0400

    completed assignment

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -3,30 +3,55 @@ package com.github.curriculeon;
 /**
  * Created by leon on 2/12/18.
  */
-public class Person {
-    public Person() {
-    }
 
-    public Person(int age) {
+/**
+ * methods implemented.
+ * Monica Deshmukh 07/16/2020
+ */
+
+public class Person {
+    private String name;
+    private Integer age;
+
+    public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+    }
+    public  Person(Integer age){
+    //public Person(int age) {
+        //this.age = (age!=0) ? age : null;
+        this.age = age;
+        this.name = null;
     }
 
     public Person(String name) {
+        this.name = name;
+        this.age = Integer.MAX_VALUE;
     }
-
-    public Person(String name, int age) {
+    public Person(String name, Integer age) {
+    //public Person(String name, int age) {
+        this.name = name;
+        //this.age = (age!=0) ? age : null;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
+    //public void setAge(int age) {
+       //this.age = (age!=0) ? age : null;
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
+        //return null;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+        //return null;
     }
 }

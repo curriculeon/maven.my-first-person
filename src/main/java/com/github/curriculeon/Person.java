e2df7d80c0a738ee9f07b8d7d5a2eee5f5c1311b
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-16 11:30:15 -0400

    Most test cases done

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,29 +4,40 @@ package com.github.curriculeon;
  * Created by leon on 2/12/18.
  */
 public class Person {
+    private int personAge;
+    private String personName;
+
     public Person() {
+
     }
 
     public Person(int age) {
+        personAge = age;
+        personName = "";
     }
 
     public Person(String name) {
+        personName = name;
     }
 
     public Person(String name, int age) {
+       personName = name;
+       personAge = age;
     }
 
     public void setName(String name) {
+        personName = name;
     }
 
     public void setAge(int age) {
+        personAge = age;
     }
 
     public String getName() {
-        return null;
+        return personName;
     }
 
     public Integer getAge() {
-        return null;
+        return personAge;
     }
 }

79e3cd7a588be5cb7c559f0fdf4e463e7ca2aae3
monicadeshmukh <renavikar_monica@yahoo.com>
2020-07-16 14:29:44 -0400

    constructors optimsed

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -17,21 +17,19 @@ public class Person {
         this.name = "";
         this.age = Integer.MAX_VALUE;
     }
+
     public  Person(Integer age){
-    //public Person(int age) {
-        //this.age = (age!=0) ? age : null;
+        this();
         this.age = age;
-        this.name = null;
     }
 
     public Person(String name) {
+        this();
         this.name = name;
-        this.age = Integer.MAX_VALUE;
     }
+
     public Person(String name, Integer age) {
-    //public Person(String name, int age) {
         this.name = name;
-        //this.age = (age!=0) ? age : null;
         this.age = age;
     }
 
@@ -40,18 +38,14 @@ public class Person {
     }
 
     public void setAge(Integer age) {
-    //public void setAge(int age) {
-       //this.age = (age!=0) ? age : null;
         this.age = age;
     }
 
     public String getName() {
         return name;
-        //return null;
     }
 
     public Integer getAge() {
         return age;
-        //return null;
     }
 }

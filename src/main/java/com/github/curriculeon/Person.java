9259e2d6dd1b4ca7f8ca734fa403f9b9610f2cec
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-07-17 14:10:00 -0400

    finished Person

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,29 +4,47 @@ package com.github.curriculeon;
  * Created by leon on 2/12/18.
  */
 public class Person {
+    String name;
+    Integer age;
+    char gender;
+    boolean isStudent;
+    boolean isParent;
+
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+
     }
 
-    public Person(int age) {
+    public Person(Integer age) {
+        this.age = age;
+        this.name = "";
     }
 
     public Person(String name) {
+        this.name = name;
+        this.age = null;
     }
 
-    public Person(String name, int age) {
+    public Person(String name, Integer age) {
+        this(name);
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
+        this.age = age;
+
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }

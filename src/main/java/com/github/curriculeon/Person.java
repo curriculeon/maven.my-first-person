34e564af9b037431b99e9e4a9c559d3d8a254ed9
Julia Waclawek <julie.waclawek@gmail.com>
2020-07-20 15:18:13 -0400

    updated

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,26 +4,24 @@ package com.github.curriculeon;
  * Created by leon on 2/12/18.
  */
 // implement a Java Class which encapsulates data about a Person object
-public class Person {
-    private String name;
-    private int age;
+public class Person { //class signature
+    private String name; //instance variable
+    private Integer age; //instance variable
 
     public Person() {
         this.name = "";
-        this.age = age;
+        this.age = Integer.MAX_VALUE;
     }
 
-    public Person(int age) {
-        this.age = age;
-        this.name = null;
+    public Person(Integer age) { //constructor signature
+        this.age = age;// setting instance variable
     }
 
-    public Person(String name) {
-        this.name = name;
-        this.age = age;
+    public Person(String name) { //constructor signature
+        this.name = name; // setting instance variable
     }
 
-    public Person(String name, int age) {
+    public Person(String name, Integer age) {
         this.name = name;
         this.age = age;
     }
@@ -32,15 +30,15 @@ public class Person {
         this.name = name;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public Integer getAge() {
-        return age;
+        return this.age;
     }
 }

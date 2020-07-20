d8efb493e6d12d792176749c9ea64f6adb536db0
Steven Snyder <sts12589@gmail.com>
2020-07-20 10:44:33 -0400

    half tests passed

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,29 +4,39 @@ package com.github.curriculeon;
  * Created by leon on 2/12/18.
  */
 public class Person {
+    String name;
+    int age;
+
     public Person() {
+
     }
 
     public Person(int age) {
+        this.setAge(age);
     }
 
     public Person(String name) {
+        this.setName(name);
     }
 
     public Person(String name, int age) {
+        this.setName(name);
+        this.setAge(age);
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age= age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }

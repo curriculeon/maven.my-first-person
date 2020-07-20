6853e0d73815fc86f26c45c943cf373fcbcfdc16
Ezra Vance <3zravance@gmail.com>
2020-07-20 10:24:23 -0400

    Fields, Getter/Setter methods implemented. Possible error in SetAgeTest.

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,6 +4,9 @@ package com.github.curriculeon;
  * Created by leon on 2/12/18.
  */
 public class Person {
+    private String name;
+    private int age;
+
     public Person() {
     }
 
@@ -17,16 +20,18 @@ public class Person {
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }

2f16947ac9243ffbc7fc2fdae428cf69c6ecd425
Brent <BrentC0328@gmail.com>
2022-08-08 19:06:59 -0400

    name, constructor. 20/29 test pass

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,29 +4,43 @@ package com.github.curriculeon;
  * Created by leon on 2/12/18.
  */
 public class Person {
+    //instance variables
+    private String theName;
+    private Integer theAge;
+
+
     public Person() {
+        this.theName = "Brent";
+        this.theAge = 30;
     }
 
     public Person(int age) {
+        this.theAge = age;
     }
 
     public Person(String name) {
+        this.theName = name;
+
     }
 
     public Person(String name, int age) {
+        this.theName = name;
+        this.theAge = age;
     }
 
     public void setName(String name) {
+        this.theName = name;
     }
 
     public void setAge(int age) {
+        this.theAge = age;
     }
 
     public String getName() {
-        return null;
+        return theName;
     }
 
     public Integer getAge() {
-        return null;
+        return theAge;
     }
 }
